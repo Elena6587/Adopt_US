8acc00fa29a7af7b9cd4e4046696f2947d2af51a
Votre Nom <votre.email@example.com>
2026-06-04 14:08:10 +0200

    Mise a jour des fichiers PHP de Adopt US

--- Connexion.php
+++ Connexion.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['user'] = 'user'; 
                 $_SESSION['user_id'] = $user['ID_adoptant'];
                 $_SESSION['user_nom'] = $user['nom'];
-                header('Location: Acceuil.php');
+                header('Location: index.php');
                 exit();
             } else {
                 $error = 'Mot de passe incorrect.';
